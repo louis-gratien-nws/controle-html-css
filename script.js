9093a15fb4f0574c77cb92b4271baaefe2f161ea
louis-gratien-nws <lgratien@normandiewebschool.frm>
2024-12-19 22:45:53 +0100

    Mise à jour des styles pour la mise à l'échelle à 100%

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const toggleButton = document.getElementById('theme-toggle');
-const body = document.body;
+    const toggleButton = document.getElementById('theme-toggle');
+    const body = document.body;
 
-toggleButton.addEventListener('click', () => {
-    body.classList.toggle('dark-mode'); 
+    toggleButton.addEventListener('click', () => {
+        body.classList.toggle('dark-mode'); 
 
-    if (body.classList.contains('dark-mode')) {
-        toggleButton.textContent = '☀️'; 
-    } else {
-        toggleButton.textContent = '🌙'; 
-    }
-});
+        if (body.classList.contains('dark-mode')) {
+            toggleButton.textContent = '☀️'; 
+        } else {
+            toggleButton.textContent = '🌙'; 
+        }
+    });
